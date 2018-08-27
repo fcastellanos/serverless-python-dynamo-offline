e2daa18d7b5202958e6f79c6ec6f22d6b47e868d
Fernando Castellanos <fcastellanos@bandsintown.com>
2018-08-27 15:39:48 -0700

    Added the delete task function

---
+++ todos/delete.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+def delete(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # delete the todo from the database
+    table.delete_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
+
+    return response
